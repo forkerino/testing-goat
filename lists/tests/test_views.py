110827e9328225849c3239ddbb2f7fe7165f4426
Pieter Stokkink <pieterstokkink@gmail.com>
2018-01-29 16:57:12 +0100

    validate unique items per list

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,6 +3,8 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.utils.html import escape
 
+from unittest import skip
+
 from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
@@ -131,3 +133,17 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
