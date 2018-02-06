f26907c3a6ea93422ecf817866a22aaf857b387c
Pieter Stokkink <pieterstokkink@gmail.com>
2018-02-06 15:42:47 +0100

    custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -126,3 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'p4818s@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
